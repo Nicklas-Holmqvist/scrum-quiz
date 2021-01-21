5ead42376ad423c1e278ec15d8e4b6aaa0389d5c
Jonas Glingert <jonas.glingert@medieinstitutet.se>
2021-01-21 23:22:49 +0100

    made border and bots-buttons smaller

--- app/app.js
+++ app/app.js
@@ -1,18 +1,18 @@
 // onClick Border-toggle
 function borderClick1() {
-    if ( botcard1.style.border == '2px dashed black' ) 
+    if ( botcard1.style.border == '1px solid black' ) 
     botcard1.style.border = '1px solid transparent'; 
-    else botcard1.style.border='2px dashed black';
+    else botcard1.style.border='1px solid black';
 }
 
 function borderClick2() {
-    if ( botcard2.style.border == '2px dashed black' ) 
+    if ( botcard2.style.border == '1px solid black' ) 
     botcard2.style.border = '1px solid transparent'; 
-    else botcard2.style.border='2px dashed black';
+    else botcard2.style.border='1px solid black';
 }
 
 function borderClick3() {
-    if ( botcard3.style.border == '2px dashed black' ) 
+    if ( botcard3.style.border == '1px solid black' ) 
     botcard3.style.border = '1px solid transparent'; 
-    else botcard3.style.border='2px dashed black';
+    else botcard3.style.border='1px solid black';
 }
