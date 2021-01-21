8603d5c548d8e8fcf53b6bcd30fabbb45d82e816
Jonas Glingert <jonas.glingert@medieinstitutet.se>
2021-01-21 23:08:54 +0100

    changed name on some buttons

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// Pro onClick
+function borderClick1() {
+    document.getElementById("botcard1").style.border = "2px dashed black";
+}
+
+function borderClick2() {
+    document.getElementById("botcard2").style.border = "2px dashed black";
+}
+
+function borderClick3() {
+    document.getElementById("botcard3").style.border = "2px dashed black";
+}
