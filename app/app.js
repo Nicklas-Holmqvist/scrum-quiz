81b6e95a69f0ffeea8a0896771619a420a19583f
Olof-Wallgren <naturel.olof@hotmail.com>
2021-01-26 14:21:54 +0100

    inputCompair

--- app/app.js
+++ app/app.js
@@ -6,11 +6,11 @@ const theGameBotColor = document.querySelector('.figure-2');
  */
 setLink.forEach((e) => {
 
-    e.addEventListener('click', () => {        
+    e.addEventListener('click', () => {
         if (e.classList.contains('bot-active')) {
             e.classList.remove('bot-active')
         }
-        else if (!e.classList.contains('bot-active')) {   
+        else if (!e.classList.contains('bot-active')) {
             setLink.forEach((i) => {
                 i.classList.remove('bot-active')
             })
@@ -29,8 +29,37 @@ setLink.forEach((e) => {
             }
         }
     })
-})  
-        
-            
-        
-  
+})
+////boten skapa ett randomnr som gämförs med spelets randomnr///////////
+const questionNum = Math.trunc(Math.random() * 20) + 1;
+const answer = document.querySelector('.answer').textContent;
+let activePlayer = 0;
+document.querySelector('#confirm').addEventListener('click', () => {
+    let input = Number(document.querySelector('#number').value);
+    if (!input) {
+        console.log('invalid number')
+    } else if (input === questionNum) {
+        document.querySelector('.answer').textContent = "Rätt nummer"
+    } else if (input !== questionNum) {
+        document.querySelector('.answer').textContent = input < questionNum ? "För lågt nummer" : "För högt "
+        activePlayer++
+    } else document.querySelector('.answer').textContent = "GAME OVER"
+
+})
+if (activePlayer === 1) {
+    ////BotCompairNum()///////
+}
+
+const BotCompairNum = function (randomNum) {
+    let botNum = Math.trunc(Math.random() * 20) + 1;
+    if (randomNum === botNum) {
+        console.log("Rätt Nummer")
+
+    } else if (randomNum !== botNum) {
+        document.querySelector('.answer').textContent = botNum > randomNum ? "För högt nummer" : "För lågt nummer"
+        activePlayer--
+    } else console.log("gameOver")
+}
+
+
+
