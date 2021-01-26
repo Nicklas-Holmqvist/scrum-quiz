5e172de5495c7419e5323fa466578ffe95aac676
Olof-Wallgren <naturel.olof@hotmail.com>
2021-01-26 21:14:07 +0100

    logik standard funkar

--- app/app.js
+++ app/app.js
@@ -30,36 +30,69 @@ setLink.forEach((e) => {
         }
     })
 })
-////boten skapa ett randomnr som gämförs med spelets randomnr///////////
+
+///////////////////////////////////////////
+////////////// STANDARDBOT LOGIK /////////
+/////////////////////////////////////////
+
+////// Globala variabler //////////////
 const questionNum = Math.trunc(Math.random() * 20) + 1;
-const answer = document.querySelector('.answer').textContent;
+const answer = document.querySelector('.answer').textContent = " "
 let activePlayer = 0;
+document.querySelector(`.player--${activePlayer}`).classList.add('activeLight')
+
+///// Jämför input med random tal //
 document.querySelector('#confirm').addEventListener('click', () => {
     let input = Number(document.querySelector('#number').value);
-    if (!input) {
-        console.log('invalid number')
+    console.log(questionNum)
+    if (!input || input > 20) {
+        document.querySelector('.answer').textContent = "Invalid nummer"
     } else if (input === questionNum) {
         document.querySelector('.answer').textContent = "Rätt nummer"
     } else if (input !== questionNum) {
         document.querySelector('.answer').textContent = input < questionNum ? "För lågt nummer" : "För högt "
-        activePlayer++
+        switchPlayer()
     } else document.querySelector('.answer').textContent = "GAME OVER"
-
 })
-if (activePlayer === 1) {
-    ////BotCompairNum()///////
+
+
+
+
+
+//////Jämför Bot med randomnummer////
+const BotCompairNum = function () {
+    if (activePlayer === 1) {
+        let botNum = Math.trunc(Math.random() * 20) + 1;
+        if (questionNum === botNum) {
+            document.querySelector('.answer').textContent = "Bot gissa rätt nummer"
+
+        } else if (questionNum !== botNum) {
+            document.querySelector('.answer').textContent = botNum > questionNum ? "För högt nummer" : "För lågt nummer"
+            document.querySelector('.show-latest-answer').textContent = `Bot guess ${botNum}` //console.log(`bot guess ${botNum}`)
+            switchPlayer()
+        } else console.log("gameOver")
+    }
 }
 
-const BotCompairNum = function (randomNum) {
-    let botNum = Math.trunc(Math.random() * 20) + 1;
-    if (randomNum === botNum) {
-        console.log("Rätt Nummer")
 
-    } else if (randomNum !== botNum) {
-        document.querySelector('.answer').textContent = botNum > randomNum ? "För högt nummer" : "För lågt nummer"
-        activePlayer--
-    } else console.log("gameOver")
+const switchPlayer = function () {
+    document.querySelector(`.player--${activePlayer}`).classList.remove('activeLight')
+    activePlayer = activePlayer === 0 ? 1 : 0
+    document.querySelector(`.player--${activePlayer}`).classList.add('activeLight')
+    setTimeout(() => {
+        BotCompairNum()
+    }, 1000 * (Math.random() * 2 + 4));
+
+}
+const botGuess = function () {
+    return Math.trunc(Math.random() * 20) + 1;
 }
 
-
-
+////////////// bot1 vinner aldgig om den får rätt siffra kör en if så att den får fel
+///////////// Bot2 mellan 1-20 standard
+//////////// Bot3 beräknar span mellan senaste svaret och högre eller lägre 
+                       ////////////
+                //////////////////////////
+//////////////// STANDARDBOT LOGIC SLUT ////////////////////
+               //////////////////////////
+                    /////////////
