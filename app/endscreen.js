a2236516f0e2d5529068fc16d9f83251bf0d036a
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-01-27 16:02:09 +0100

    added endscreen functions

--- app/endscreen.js
+++ app/endscreen.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', main)
 
 function main() {
-    
+    showGameSummary();    
 }
 
 // Globala variabler för spelaren
@@ -11,3 +11,10 @@ let playerWins = parseInt(localStorage.getItem("player-wins"))
 let playerGames = parseInt(localStorage.getItem("player-games"))
 let playerGuesses = parseInt(localStorage.getItem("player-guesses"))
 
+function showGameSummary() {
+    document.querySelector('#player').innerHTML = "Grattis " + playerName + "!"
+    document.querySelector('#guesses').innerHTML = "Du vann på " + playerGuesses + " gissningar."
+    document.querySelector('#games').innerHTML = "Antal spelade spel: " + playerGames
+    document.querySelector('#wins').innerHTML = "Antal vunna spel: " + playerWins
+
+}
