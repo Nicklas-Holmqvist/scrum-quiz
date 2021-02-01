12a13186a9c13c9ba3780186c38cd6e6fbe7c44f
Jonas Glingert <jonas.glingert@medieinstitutet.se>
2021-02-01 12:24:02 +0100

    merged maries branch

--- app/endscreen.js
+++ app/endscreen.js
@@ -21,9 +21,9 @@ let highscore = JSON.parse(localStorage.getItem("highscore"))
  */
 function showGameSummary() {
     document.querySelector('#player').innerHTML = "Grattis " + playerName + "!"
-    document.querySelector('#guesses').innerHTML = "Du vann på " + playerGuesses + " gissningar."
-    document.querySelector('#games').innerHTML = "Antal spelade spel: " + playerGames
-    document.querySelector('#wins').innerHTML = "Antal vunna spel: " + playerWins    
+    document.querySelector('#guesses').innerHTML = playerGuesses
+    document.querySelector('#games').innerHTML = playerGames
+    document.querySelector('#wins').innerHTML = playerWins      
 }
 
 /**
