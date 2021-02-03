c3445692025a51d3bc3f3550d8992ee06c650432
Jonas Glingert <jonas.glingert@medieinstitutet.se>
2021-02-03 15:57:24 +0100

    fixed two spellings

--- app/stats.js
+++ app/stats.js
@@ -3,7 +3,7 @@ function getBotStats() {
     
     let fetchLSBotColor = JSON.parse(localStorage.getItem("bot"))
 
-    document.querySelector("#easyWins").innerHTML = `Vunna matsscher: ${fetchLSBotColor.easyBot.wins}`
+    document.querySelector("#easyWins").innerHTML = `Vunna matcher: ${fetchLSBotColor.easyBot.wins}`
     document.querySelector("#easyGames").innerHTML = `Spelade matcher: ${fetchLSBotColor.easyBot.games}`
     
     document.querySelector("#mediWins").innerHTML = `Vunna matcher: ${fetchLSBotColor.mediBot.wins}`
