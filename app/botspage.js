70cc3953d01449bd8708dc0eb507d57582bb61d8
Yohan Kazuya Risteski <69914494+yohan256@users.noreply.github.com>
2021-02-03 12:40:48 +0100

    bot-history-fix
    fixed-bot-history

--- app/botspage.js
+++ app/botspage.js
@@ -29,6 +29,8 @@ let bot = {
  */
 activeBot.forEach((e) => {
 
+    let history = JSON.parse(localStorage.getItem("bot"))
+
     e.addEventListener('click', () => {
         if (e.classList.contains('bot-active')) {
             e.classList.remove('bot-active')
@@ -38,33 +40,56 @@ activeBot.forEach((e) => {
                 i.classList.remove('bot-active')
             })
             e.classList.add('bot-active')
-
+ 
             if (e.classList.contains('figure-green')) {
                 // botColor = 1;
                 // easyBot.botColor = 1
                 // easyBot.wins = 0
                 // easyBot.games = 0 
                 bot.botColor = 1
-                localStorage.setItem("bot", JSON.stringify(bot))
+               
+                if (history) {
+                    history.botColor = 1
+                    localStorage.setItem("bot", JSON.stringify(history))
+                }
+                else {
+                    localStorage.setItem("bot", JSON.stringify(bot))
+                }
+                
             }
-
+ 
             else if (e.classList.contains('figure-blue')) {
                 // botColor = 2;
                 // mediBot.botColor = 2
                 // mediBot.wins = 0
                 // mediBot.games = 0 
                 bot.botColor = 2
-                localStorage.setItem("bot",  JSON.stringify(bot))
+                
+                if (history) {
+                   
+                    history.botColor = 2
+                    localStorage.setItem("bot", JSON.stringify(history))
+                }
+                else {
+                    localStorage.setItem("bot", JSON.stringify(bot))
+                }
+ 
             }
-
+ 
             else if (e.classList.contains('figure-red')) {
                 // botColor = 3;
                 // hardBot.botColor = 3
                 // hardBot.wins = 0
-                // hardBot.games = 0 
+                // hardBot.games = 0
                 bot.botColor = 3
-                localStorage.setItem("bot",  JSON.stringify(bot))
+                if (history) {
+                    history.botColor =3
+                    localStorage.setItem("bot", JSON.stringify(history))
+                }
+                else {
+                    localStorage.setItem("bot", JSON.stringify(bot))
+                }
             }
         }
     })
-})  
+}) 
